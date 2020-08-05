8aa212c9b00272f1b37d2724cc3e021c3881f5f6
鹏 王 <1160510796@qq.com>
2020-08-05 16:45:54 +0800

    update API

---
+++ DotNetCore/WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public AuthController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        static IConfiguration Configuration { get; set; }
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Get(string userName, string pwd)
+        {
+            if (!string.IsNullOrEmpty(userName) && !string.IsNullOrEmpty(pwd))
+            {
+                //添加身份认证方案
+                var jwtConfig = Configuration.GetSection("Jwt").Get<JwtConfig>();
+
+                var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Nbf,$"{new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds()}") ,
+                    new Claim (JwtRegisteredClaimNames.Exp,$"{new DateTimeOffset(DateTime.Now.AddMinutes(30)).ToUnixTimeSeconds()}"),
+                    new Claim(ClaimTypes.Name, userName)
+                };
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfig.SigningKey));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                var token = new JwtSecurityToken(
+                    issuer: jwtConfig.Issuer,
+                    audience: jwtConfig.Audience,
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds);
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+            else
+            {
+                return BadRequest(new { message = "username or password is incorrect." });
+            }
+        }
+    }
+}
--- DotNetCore/WebAPI/Startup.cs
+++ DotNetCore/WebAPI/Startup.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Common.Model;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace WebAPI
 {
@@ -28,49 +31,33 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSession();
-            //services.AddMvc(o =>
-            //{
-            //    o.Filters.Add(typeof(MyExceptionFilterAttribute));// 全局异常Filter  
-            //}).AddRazorRuntimeCompilation();
+
             //添加身份认证方案
             var jwtConfig = Configuration.GetSection("Jwt").Get<JwtConfig>();
-            services.AddAuthentication
-                (authoption =>
-                {
-                    //指定默认选项
-                    authoption.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                    authoption.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                    authoption.DefaultSignOutScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                    authoption.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                })
-           .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, option =>
-           {
-               option.Cookie.Name = "adCookie";//设置存储用户登录信息（用户Token信息）的Cookie名称
-               option.Cookie.HttpOnly = true;//设置存储用户登录信息（用户Token信息）的Cookie，无法通过客户端浏览器脚本(如JavaScript等)访问到
-               option.ExpireTimeSpan = TimeSpan.FromDays(3);// 过期时间
-               option.SlidingExpiration = true;// 是否在过期时间过半的时候，自动延期
-               option.LoginPath = "/Account/Login";
-               option.LogoutPath = "/Account/LoginOut";
-           }); 
-           //.AddJwtBearer(option =>
-           //{
-           //    option.TokenValidationParameters = new TokenValidationParameters
-           //    {
-           //        ValidIssuer = jwtConfig.Issuer,
-           //        ValidAudience = jwtConfig.Audience,
-           //        ValidateIssuer = true,
-           //        ValidateLifetime = jwtConfig.ValidateLifetime,
-           //        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfig.SigningKey)),
-           //        //缓冲过期时间，总的有效时间等于这个时间加上jwt的过期时间
-           //        ClockSkew = TimeSpan.FromSeconds(0)
-           //    };
-           //});
+            //添加jwt验证：
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,//是否验证Issuer
+                        ValidateAudience = true,//是否验证Audience
+                        ValidateLifetime = true,//是否验证失效时间
+                        ClockSkew = TimeSpan.FromSeconds(30),
+                        ValidateIssuerSigningKey = true,//是否验证SecurityKey
+                        ValidAudience = jwtConfig.Audience,//Audience
+                        ValidIssuer = jwtConfig.Issuer,//Issuer，这两项和前面签发jwt的设置一致
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfig.SigningKey))//拿到SecurityKey
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            ///添加jwt验证
+            app.UseAuthentication();
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
